5b942c013a143ff992c82becfac13bab1476f201
egor <Egor78396@gmail.com>
2024-05-30 17:25:59 +0300

    Pastebin(api gateway communication)

--- Pastebin/user-server/src/main/java/org/pastebin/application/api_server/UserServerApplication.java
+++ Pastebin/user-server/src/main/java/org/pastebin/application/api_server/UserServerApplication.java
@@ -2,13 +2,12 @@ package org.pastebin.application.api_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class ApiServerApplication {
+public class UserServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiServerApplication.class, args);
+        SpringApplication.run(UserServerApplication.class, args);
     }
 
 }
