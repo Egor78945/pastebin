1c95d7adcf55cd35392ec426f52c89c1aef33d44
egor <Egor78396@gmail.com>
2024-06-10 17:33:10 +0300

    Pastebin(db-server logging)

--- Pastebin/db-server/src/main/java/org/pastebin/application/db_server/controllers/controller_advices/DatabaseControllerAdvice.java
+++ Pastebin/db-server/src/main/java/org/pastebin/application/db_server/controllers/controller_advices/DatabaseControllerAdvice.java
@@ -1,5 +1,6 @@
 package org.pastebin.application.db_server.controllers.controller_advices;
 
+import lombok.extern.slf4j.Slf4j;
 import org.pastebin.application.db_server.annotations.DatabaseControllerExceptionHandler;
 import org.pastebin.application.db_server.exceptions.RequestCancelledException;
 import org.springframework.http.HttpStatus;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice(annotations = DatabaseControllerExceptionHandler.class)
+@Slf4j
 public class DatabaseControllerAdvice {
     @ExceptionHandler(RequestCancelledException.class)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Integer> requestCancelledExceptionHandler() {
+    public ResponseEntity<Integer> requestCancelledExceptionHandler(Exception e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(0, HttpStatus.OK);
     }
 }
