3328396ef6faff972d9e4810b905b64d1f19a11a
egor <Egor78396@gmail.com>
2024-06-11 22:35:02 +0300

    Pastebin(fixed logging)

--- Pastebin/user-server/src/main/java/org/pastebin/application/user_server/services/apects/MessageServiceAspect.java
+++ Pastebin/user-server/src/main/java/org/pastebin/application/user_server/services/apects/MessageServiceAspect.java
@@ -3,6 +3,7 @@ package org.pastebin.application.user_server.services.apects;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -10,26 +11,26 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class MessageServiceAspect {
-    @After("execution(public Long postMessage(java.lang.String))")
+    @AfterReturning("execution(public Long postMessage(java.lang.String))")
     public void afterPostMessageAdvice(JoinPoint joinPoint) {
         String message = (String) joinPoint.getArgs()[0];
-        log.info(String.format("Message service: Message have been sent to save: %s", message));
+        log.info(String.format("Message service: Message have been saved: %s", message));
     }
 
-    @After("execution(public java.lang.Integer getMessageHash(java.lang.Long))")
-    public void afterGetMessageHashAdvice(JoinPoint joinPoint) {
+    @AfterReturning("execution(public java.lang.Integer getMessageHash(java.lang.Long))")
+    public void afterReturningGetMessageHashAdvice(JoinPoint joinPoint) {
         Long messageHashId = (Long) joinPoint.getArgs()[0];
         log.info(String.format("Message service: Message hash with id %s have been gotten", messageHashId));
     }
 
-    @After("execution(public java.lang.String getMessage(java.lang.Long))")
-    public void afterGetMessageAdvice(JoinPoint joinPoint) {
+    @AfterReturning("execution(public java.lang.String getMessage(java.lang.Long))")
+    public void afterReturningGetMessageAdvice(JoinPoint joinPoint) {
         Long messageHashId = (Long) joinPoint.getArgs()[0];
         log.info(String.format("Message service: Message with message hash with id %s have been gotten", messageHashId));
     }
 
-    @After("execution(public java.lang.String buildPersonalReference(java.lang.Long))")
-    public void afterBuildPersonalReferenceAdvice(JoinPoint joinPoint) {
+    @AfterReturning("execution(public java.lang.String buildPersonalReference(java.lang.Long))")
+    public void afterReturningBuildPersonalReferenceAdvice(JoinPoint joinPoint) {
         Long messageHashId = (Long) joinPoint.getArgs()[0];
         log.info(String.format("Message service: Personal reference for message with message hash with id %s have been build", messageHashId));
     }
